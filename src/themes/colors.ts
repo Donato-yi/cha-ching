d3cdb6f5f8fe2ad6f5ba401ff457ec5b247d7e36
Donato Yi <donatoyi@protonmail.com>
2018-04-19 12:34:01 +0900

    feat(theme): add new colors
    - lightGreen: 98fb98
    - weakBlack: 'rgba(0,0,0,0.8)',
    - weakWhite: 'rgba(255,255,255,0.8)',

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,8 +1,11 @@
 const colors = {
   black: 'rgb(0,0,0)',
+  lightGreen: '#98fb98',
   tintGreen: 'rgb(100,255,100)',
   transparent: 'rgba(0,0,0,0.0)',
   warmGrey: 'rgb(151,151,151)',
+  weakBlack: 'rgba(0,0,0,0.8)',
+  weakWhite: 'rgba(255,255,255,0.8)',
   white: 'rgb(255,255,255)',
 }
 
