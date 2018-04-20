ca528dcab0f4e54ced461302412ca2d546bdc976
Donato Yi <donatoyi@protonmail.com>
2018-04-21 00:15:32 +0900

    feat(theme): add colors
      blue: 'rgb(12,140,252)',
      lightBlue: 'rgb(188,224,254)',
      whiteBlue: 'rgb(241,249,255)',
      weakWhite: 'rgba(255,255,255,0.2)',

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,12 +1,15 @@
 const colors = {
   black: 'rgb(0,0,0)',
+  blue: 'rgb(12,140,252)',
+  lightBlue: 'rgb(188,224,254)',
   lightGreen: '#98fb98',
   tintGreen: 'rgb(100,255,100)',
   transparent: 'rgba(0,0,0,0.0)',
   warmGrey: 'rgb(151,151,151)',
   weakBlack: 'rgba(0,0,0,0.8)',
-  weakWhite: 'rgba(255,255,255,0.8)',
+  weakWhite: 'rgba(255,255,255,0.2)',
   white: 'rgb(255,255,255)',
+  whiteBlue: 'rgb(241,249,255)',
 }
 
 export default colors
