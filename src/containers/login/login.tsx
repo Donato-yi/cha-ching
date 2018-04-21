962d50120d92fde27645faf99f8d3040ec36f5a9
Donato Yi <donatoyi@protonmail.com>
2018-04-22 01:03:30 +0900

    fix(typo): update code style

--- src/containers/login/login.tsx
+++ src/containers/login/login.tsx
@@ -20,7 +20,7 @@ class Login extends React.Component<LoginScreenProps, LoginScreenState> {
   }
 
   toMain = () => {
-    this.props.navigation.navigate('main')
+    this.props.navigation.navigate('home')
   }
 
   render() {
