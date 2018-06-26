97b3284b3ef7d4b933f39428845b2b570a5d4b2e
Donato Yi <donatoyi@protonmail.com>
2018-06-27 06:56:33 +0900

    refactor(server): update app config to use staging env

--- src/config/setting.ts
+++ src/config/setting.ts
@@ -1,10 +1,14 @@
 import { equals } from 'ramda'
 
-const SERVER_ENV = 'development' // SERVER_ENV = 'production', 'staging', 'development'
+const SERVER_ENV = 'staging' // SERVER_ENV = 'production', 'staging', 'development'
 const configurations = {
   production: {},
-  staging: {},
-  development: {},
+  staging: {
+    baseURL: 'http://18.219.115.82/',
+  },
+  development: {
+    baseURL: 'localhost:3000',
+  },
 }
 
 const setting = {
